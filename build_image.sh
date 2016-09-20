a0195c7995cc8ba8f387c3a729031aa4198aea80
hitsumabushi <hitsumabushi.org@gmail.com>
2016-09-20 23:14:41 +0900

    update build script

--- build_image.sh
+++ build_image.sh
@@ -10,13 +10,13 @@ fi
 
 IMAGE_BUILD_DATE=$(date --iso-8601=seconds)
 IMAGE_VCS_REF=$(git rev-parse HEAD)
-PHPIPAM_VERSION=$(cat PHPIPAM_VERSION)
+PHPIPAM_VERSION=$(cat PHPIPAM_VERSION | tr -d ' ' | tr -d '\n')
 
- docker build \
-   --build-arg "IMAGE_BUILD_DATE=\"${IMAGE_BUILD_DATE}\"" \
-   --build-arg "IMAGE_VCS_REF=\"${IMAGE_VCS_REF}\"" \
-   --build-arg "PHPIPAM_VERSION=\"${PHPIPAM_VERSION}\"" \
-   ${BUILD_ARGS} \
-   -t ${TAG} \
-   -f dockerimages/phpipam/Dockerfile \
-   dockerimages/phpipam/
+docker build \
+  --build-arg "IMAGE_BUILD_DATE=${IMAGE_BUILD_DATE}" \
+  --build-arg "IMAGE_VCS_REF=${IMAGE_VCS_REF}" \
+  --build-arg "PHPIPAM_VERSION=${PHPIPAM_VERSION}" \
+  ${BUILD_ARGS} \
+  -t ${TAG} \
+  -f dockerimages/phpipam/Dockerfile \
+  dockerimages/phpipam/
